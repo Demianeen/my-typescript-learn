72855b7190f9dc68d70e623b0d672803079930ff
Demian <dnetlyukh@gmail.com>
2022-06-24 14:53:58 +0300

    Delete build directory

--- build/test11-form_validation/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const form = {
-    username: 'Demian',
-    password: 'Ader'
-};
-const formValidation = {
-    username: { isValid: true },
-    password: { isValid: false, errorMessage: 'Some error' }
-};
